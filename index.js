9d9e909533908778679b0ec30668b879cf7136f6
Olukeye <seunolukeye2000@gmail.com>
2021-08-19 20:02:23 +0100

    make it better

--- index.js
+++ index.js
@@ -61,13 +61,13 @@ app.use('/api/v1', indexRouter);
 
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html')))
-} else {
-  app.get('/', (req, res) => { res.send('API is running') })
-}
+//   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html')))
+// } else {
+//   app.get('/', (req, res) => { res.send('API is running') })
+// }
 
 app.get('/', (_req, res) => {
   res.redirect('/api/v1');
